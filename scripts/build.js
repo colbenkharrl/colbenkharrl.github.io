fd36f070318adbc0b9fda4cc3f2877b32d6c565b
Colben Kharrl <colbenkharrl@gmail.com>
2017-08-27 19:11:19 -0700

    redesign

---
+++ scripts/build.js
@@ -0,0 +1,64 @@
+var boxData, width, even, tHeight;
+var occs = ["programmer", "designer", "prototyper", "creator", "data scientist", "student", "developer", "thinker", "gamer", "writer", "consultant", "communicator"];
+
+$(window).on("resize", function() {
+	render();
+});
+
+var svg = d3.select(".top-content").append("svg").attr("width", "100%").attr("height", "100%"),
+	tGroup = svg.selectAll(".content"),
+	aGroup = svg.append("g").selectAll(".about");
+
+d3.json("data/boxes.json", function(e, d) {
+	if (e) throw e;
+	render();
+	window.setInterval(function(){
+		$("#occ").text(occs[getRandomInt(0, occs.length)]);
+	  }, 2000);
+})
+
+function render() {
+	width = $(".container-fluid").width();
+	$(".top-content").height(Math.min(500, Math.max(width)));
+	tHeight = $(".top-content").height();
+	console.log(tHeight);
+	even = width > 960;
+
+	buildTopContent();
+}
+
+function buildTopContent() {
+	// DATA JOIN
+	tGroup = tGroup.data([{i:"images/glasses.png",text:"I am a <tspan id='occ'>programmer</tspan>."}]);
+
+	// ENTER
+	ntc = tGroup.enter().append("g")
+		.attr("class", "top-content-group");
+
+	ntc.append("image")
+		.attr("x:href", function(d){return d.i;});
+
+	ntc.append("text")
+		.html(function(d){return d.text;})
+		.attr("class", "top-text");
+
+	tGroup = tGroup.merge(ntc);
+
+	tGroup
+		.attr("transform", "translate(" + (width * 0.1) + "," + (tHeight * 0.1) + ")");
+
+	tGroup.select("image")
+		.attr("width", width * 0.8)
+		.attr("height", tHeight * 0.5);
+	
+	tGroup.select("text")
+		.attr("x", (width * 0.8) / 2)
+		.attr("y", tHeight * 0.6)
+		.attr("font-size", Math.min(40, width * 0.1));
+}
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+  }
